794e297b1b7e8a05d8cf91bc8118a2eea44c80d4
FanniM <marco.fanni@live.it>
2022-03-10 22:28:21 +0100

    Fix serve

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/api-playground'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/api-playground/index.html'));});
+const app = express();
+app.use(requireHTTPS);
+app.use(express.static('./dist/api-playground'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/api-playground/'});
+});
+
 app.listen(process.env.PORT || 8080);
