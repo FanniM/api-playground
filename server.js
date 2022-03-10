d8089d8acb6256c90952cbe96721f67ddace4c60
FanniM <marco.fanni@live.it>
2022-03-10 22:19:58 +0100

    Fix server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/api-playground'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<api-playground>/index.html'));});
+'/dist/api-playground/index.html'));});
 app.listen(process.env.PORT || 8080);
